505a3a69803addb061670684a543468cb6bda123
bian-hengwei <55875341+bian-hengwei@users.noreply.github.com>
2021-03-29 17:59:05 +0800

    update README.md

--- generate_content.py
+++ generate_content.py
@@ -3,12 +3,11 @@ import re
 
 # 自动生成目录页
 if __name__ == '__main__':
-    content = '# 目录  \n  \n'
+    content = '# LeetCode  \n## 目录  \n  \n'
 
     all_files = os.listdir('./docs')
-    all_files.remove('Content.md')
     for file in sorted(all_files, key=lambda _: int(re.search('^(\d+)\..*\.md$', _)[1])):
         content += '[{}]({})  \n'.format(file[:-3], file)
 
-    with open('./docs/Content.md', 'w', encoding='utf-8') as f:
+    with open('./README.md', 'w', encoding='utf-8') as f:
         f.write(content)
