ad4b431323a27e51f3951770693f1adf184ac83c
bian-hengwei <55875341+bian-hengwei@users.noreply.github.com>
2021-04-08 23:34:51 +0800

    22

---
+++ codes/22.括号生成.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=javascript
+ *
+ * [22] 括号生成
+ */
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+ var generateParenthesis = function(n) {
+    arr = [];
+    backtrack = function(n, l, r, cur) {
+        if (n * 2 == l + r) arr.push(cur);
+        else {
+            if (l < n) backtrack(n, l+1, r, cur+"(", arr);
+            if (r < l) backtrack(n, l, r+1, cur+")", arr);
+        }
+    }
+    backtrack(n, 0, 0, "")
+    return arr;
+};
+// @lc code=end
+
