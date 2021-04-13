c5d7b4dbdc873eb85b54499012e0a6e8ee9786b1
bian-hengwei <55875341+bian-hengwei@users.noreply.github.com>
2021-04-13 19:52:06 +0800

    readme

--- generate_content.py
+++ generate_content.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     all_markdowns = os.listdir('./docs')
     all_solutions = os.listdir('./codes')
-    content = '# LeetCode\n已完成{}题  \n|序号|标题|代码|\n|:-:|:-:|:-:|\n'.format(len(all_markdowns))
+    content = '# LeetCode\n已完成{}题  \n|序号|标题|代码|\n|:-:|:-|:-:|\n'.format(len(all_markdowns))
     for file in sorted(all_markdowns, key=lambda _: int(re.search('^(\d+)\..*\.md$', _)[1])):
         ord_title = file.split('.')
         sols = sorted([sol for sol in all_solutions if sol.split('.')[0] == ord_title[0]])
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         for i, sol in enumerate(sols):
             content += '|-|-|'if i != 0 else '|{}|[{}](./docs/{})|'.format(ord_title[0], ord_title[1], file)
             content += '[{}](./codes/{})|\n'.format(LANGUAGES[sol.split('.')[2]], sol)
-    content += '  \n[:trollface:](./REWRITE.md)'
+    content += '|:trollface:|[:trollface:](./REWRITE.md)|:trollface:|'
 
     with open('./README.md', 'w', encoding='utf-8') as f:
         f.write(content)
