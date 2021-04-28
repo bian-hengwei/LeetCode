22a3b25f9aeaffa9a9efb0281686209d33c3c62d
bian-hengwei <55875341+bian-hengwei@users.noreply.github.com>
2021-04-28 13:03:54 +0800

    633

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # LeetCode
-已完成36题
+已完成37题
 [:trollface:](./REWRITE.md)
 |序号|标题|代码|
 |:-:|:-:|:-:|
@@ -44,12 +44,13 @@
 |[91](https://leetcode-cn.com/problems/decode-ways/description/)|[解码方法](./docs/91.解码方法.md)|[JavaScript](./codes/91.解码方法.js)|
 |[368](https://leetcode-cn.com/problems/largest-divisible-subset/description/)|[最大整除子集](./docs/368.最大整除子集.md)|[C++](./codes/368.最大整除子集.cpp)|
 |[377](https://leetcode-cn.com/problems/combination-sum-iv/description/)|[组合总和-ⅳ](./docs/377.组合总和-ⅳ.md)|[C++](./codes/377.组合总和-ⅳ.cpp)|
+|[633](https://leetcode-cn.com/problems/sum-of-square-numbers/description/)|[平方数之和](./docs/633.平方数之和.md)|[C++](./codes/633.平方数之和.cpp)|
 |[897](https://leetcode-cn.com/problems/increasing-order-search-tree/description/)|[递增顺序查找树](./docs/897.递增顺序查找树.md)|[C++](./codes/897.递增顺序查找树.cpp)|
 |[938](https://leetcode-cn.com/problems/range-sum-of-bst/description/)|[二叉搜索树的范围和](./docs/938.二叉搜索树的范围和.md)|[C++](./codes/938.二叉搜索树的范围和.cpp)|
 ## 语言统计
 |语言|题解|总数|
 |:-:|:-:|:-:|
 |[JavaScript](https://github.com/bian-hengwei/LeetCode/search?l=js)|12|14|
-|[C++](https://github.com/bian-hengwei/LeetCode/search?l=cpp)|10|11|
+|[C++](https://github.com/bian-hengwei/LeetCode/search?l=cpp)|11|12|
 |[Java](https://github.com/bian-hengwei/LeetCode/search?l=java)|7|9|
 |[Python](https://github.com/bian-hengwei/LeetCode/search?l=python)|7|9|
--- generate_content.py
+++ generate_content.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     URLBASE = 'https://github.com/bian-hengwei/LeetCode/search?l='
 
     # markdown file names (sorted)
+    # re for ^(number).ANYSTRING.md$
     all_markdowns = os.listdir('./docs')
     all_markdowns.sort(key=lambda _: int(re.search('^(\d+)\..*\.md$', _)[1]))
     
@@ -18,7 +19,7 @@ if __name__ == '__main__':
     all_solutions = os.listdir('./codes')
     
     # README.md
-    readme = '# LeetCode\n已完成{}题\n[:trollface:](./REWRITE.md)\n|序号|标题|代码|\n|:-:|:-:|:-:|\n'.format(len(all_markdowns))
+    readme = f'# LeetCode\n已完成{len(all_markdowns)}题\n[:trollface:](./REWRITE.md)\n|序号|标题|代码|\n|:-:|:-:|:-:|\n'
     rewrite = '# Rewrite\n[返回目录](./README.md)\n|序号|标题|代码|\n|:-:|:-:|:-:|\n'
 
     # loop in order
@@ -48,20 +49,21 @@ if __name__ == '__main__':
 
         # add lines
         for i, sol in enumerate(sols):
+            lang = LANGUAGES[sol.split('.')[2]]
             counts[sol.split('.')[2]][1] += 1
             if i == 0: counts[sol.split('.')[2]][0] += 1
-            readme += '|-|-|' if i != 0 else '|[{}]({})|[{}](./docs/{})|'.format(ord_title[0], link, title, file)
-            readme += '[{}](./codes/{})|\n'.format(LANGUAGES[sol.split('.')[2]], sol)
+            readme += '|-|-|' if i != 0 else f'|[{ord_title[0]}]({link})|[{title}](./docs/{file})|'
+            readme += f'[{lang}](./codes/{sol})|\n'
             if rewrite_flag:
-                rewrite += '|-|-|' if i != 0 else '|[{}]({})|[{}](./docs/{})|'.format(ord_title[0], link, title, file)
-                rewrite += '[{}](./codes/{})|\n'.format(LANGUAGES[sol.split('.')[2]], sol)
+                rewrite += '|-|-|' if i != 0 else f'|[{ord_title[0]}]({link})|[{title}](./docs/{file})|'
+                rewrite += f'[{lang}](./codes/{sol})|\n'
     
     # languages stats table
     readme += '## 语言统计\n|语言|题解|总数|\n|:-:|:-:|:-:|\n'
     for lang in sorted(counts.items(), key=lambda item: item[1], reverse=True):
         print(lang)
-        if (lang[0] == 'py'): readme += '|[{}]({})|{}|{}|\n'.format(LANGUAGES[lang[0]], URLBASE + 'python', lang[1][0], lang[1][1])
-        else: readme += '|[{}]({})|{}|{}|\n'.format(LANGUAGES[lang[0]], URLBASE + lang[0], lang[1][0], lang[1][1])
+        if (lang[0] == 'py'): readme += f'|[{LANGUAGES[lang[0]]}]({URLBASE}python)|{lang[1][0]}|{lang[1][1]}|\n'
+        else: readme += f'|[{LANGUAGES[lang[0]]}]({URLBASE}{lang[0]})|{lang[1][0]}|{lang[1][1]}|\n'
 
     # write to README.md
     with open('./README.md', 'w', encoding='utf-8') as f:
